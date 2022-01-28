da7a8c10952a23e4bf4492d87c920957883704c0
Jesus Herney Sanchez Rodriguez <jsanchez@indigo.tech>
2022-01-28 07:32:12 -0500

    Correct build

--- src/components/Inputs/Input.jsx
+++ src/components/Inputs/Input.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import classNames from 'classnames';
 import { passWordRegexTest } from '../../utils/Auth/auth';
 import  './InputStyles.css';
--- src/pages/Login/LoginPage.jsx
+++ src/pages/Login/LoginPage.jsx
@@ -1,5 +1,5 @@
 import classNames from 'classnames';
-import React, { useEffect, useState } from 'react';
+import React, { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { 
   Button, 
