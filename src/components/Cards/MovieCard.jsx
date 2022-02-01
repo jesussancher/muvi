26b9e3628b970fbe7939f232005d78c9a5d3f909
Jesus Herney Sanchez Rodriguez <jsanchez@indigo.tech>
2022-02-01 01:07:51 -0500

    Correct Building

--- src/components/Cards/MovieCard.jsx
+++ src/components/Cards/MovieCard.jsx
@@ -4,11 +4,9 @@ import { Icon } from '..';
 import { addToFavoriteList, removeFromFavoriteList } from '../../utils/Misc/favorites';
 import PosterFallback from '../../assets/images/fallback-poster.jpg';
 import './CardsStyles.css';
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function MovieCard(props) {
-
-    const navigate = useNavigate();
 
     const {
         id,
