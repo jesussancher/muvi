eb800a92ed823a36a36e51bea6fd1541cdc5949f
Jesus Herney Sanchez Rodriguez <jsanchez@indigo.tech>
2022-02-02 14:49:03 -0500

    Rigthts Year Autoset.

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -8,7 +8,7 @@ function Footer() {
         <MuviLogo />
         <p>Muvi is part of <a href="https://57blocks.io/" target={'_blank'} rel={'noreferrer'}>57Blocks</a></p>
         <p>Designed and developed by <a href="https://github.com/jesussancher/" target={'_blank'} rel={'noreferrer'}>Jesús Sánchez</a></p>
-        <p>All rights Reserved. © 2022 Muvi, Inc.</p>
+        <p>All rights Reserved. © {new Date().getFullYear()} Muvi, Inc.</p>
     </footer>
 }
 
