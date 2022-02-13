3ae93ab994209fd1931c4aa1dc1d9ac8b1fc9bbb
Jesus Herney Sanchez Rodriguez <jsanchez@indigo.tech>
2022-02-12 22:52:08 -0500

    Footer updated

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -6,8 +6,8 @@ function Footer() {
 
     return <footer className={'flex-column flex-center'}>
         <MuviLogo />
-        <p>Muvi is part of <a href="https://57blocks.io/" target={'_blank'} rel={'noreferrer'}>57Blocks</a></p>
-        <p>Designed and developed by <a href="https://github.com/jesussancher/" target={'_blank'} rel={'noreferrer'}>Jesús Sánchez</a></p>
+        {/* <p>Muvi is part of <a href="https://57blocks.io/" target={'_blank'} rel={'noreferrer'}>57Blocks</a></p> */}
+        <p>Designed and developed by <a href="https://twitter.com/jesussancher/" target={'_blank'} rel={'noreferrer'}>Jesús Sánchez</a></p>
         <p>All rights Reserved. © 2022 Muvi, Inc.</p>
     </footer>
 }
